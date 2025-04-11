39f0986e0ea676788504133fc55216a6a6048bbe
Aly CAMARA <Aly.CAMARA@hardis-group.com>
2025-04-11 12:41:10 +0200

    Initial commit

---
+++ src/main/java/om/camara/animalmarketplace/AnimalMarketplacePlatformApplication.java
@@ -0,0 +1,13 @@
+package om.camara.animalmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalMarketplacePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalMarketplacePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/om/camara/animalmarketplace/AnimalMarketplacePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package om.camara.animalmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalMarketplacePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
