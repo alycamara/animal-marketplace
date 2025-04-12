7366589c140e17bb93553a90283cf9a42bbe4cd3
Aly CAMARA <Aly.CAMARA@hardis-group.com>
2025-04-13 00:07:47 +0200

    desactive la security

--- src/main/java/om/camara/animalmarketplace/config/SecurityConfig.java
+++ src/main/java/om/camara/animalmarketplace/config/SecurityConfig.java
@@ -2,7 +2,10 @@ package om.camara.animalmarketplace.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,12 +14,23 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/ads/**").permitAll() // Autoriser l'accès aux pages des annonces
-                .anyRequest().authenticated() // Authentification pour les autres pages
-            )
-            .csrf(csrf -> csrf.disable()); // Désactiver CSRF avec la nouvelle API
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll() // Authentification requise pour toutes les routes
+                )
+                .csrf(csrf -> csrf.disable()); // Désactiver CSRF pour simplifier les tests
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER");
     }
 }
