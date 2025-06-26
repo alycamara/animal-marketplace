444d84c7787a05831d36e5ceb9f1d068fc250016
Aly CAMARA <Aly.CAMARA@hardis-group.com>
2025-06-26 08:00:10 +0200

    Changement de la base vers RDS AMAZON

--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "The app is starting"
-exec java -jar "animalMarketplacePlatformApplication.jar" --spring.profiles.active=${SPRING_ACTIVE_PROFILES}
--- src/main/java/com/camara/animalmarketplace/config/SecurityConfig.java
+++ src/main/java/com/camara/animalmarketplace/config/SecurityConfig.java
@@ -2,12 +2,8 @@ package com.camara.animalmarketplace.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -15,31 +11,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll() // Authentification requise pour toutes les routes
-                )
-                .csrf(csrf -> csrf.disable()); // Désactiver CSRF pour simplifier les tests
-
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll() // Autoriser toutes les requêtes
+            )
+            .csrf(csrf -> csrf.disable()); // Désactiver la protection CSRF (à utiliser avec précaution)
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER");
-    }
-
-    @Bean
-    public CharacterEncodingFilter characterEncodingFilter() {
-        CharacterEncodingFilter filter = new CharacterEncodingFilter();
-        filter.setEncoding("UTF-8");
-        filter.setForceEncoding(true);
-        return filter;
     }
 }
