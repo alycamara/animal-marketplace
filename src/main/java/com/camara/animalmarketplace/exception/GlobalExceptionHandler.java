337ed53f50ff2f744d9b371af378bdca9746d2c1
ACAM <camaraaly7@gmail.com>
2025-06-28 16:01:29 +0200

    Mis à jour

--- src/main/java/com/camara/animalmarketplace/exception/GlobalExceptionHandler.java
+++ src/main/java/com/camara/animalmarketplace/exception/GlobalExceptionHandler.java
@@ -1,9 +1,8 @@
 package com.camara.animalmarketplace.exception;
 
-import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -30,7 +29,7 @@ public class GlobalExceptionHandler {
     public String handleResourceNotFoundException(ResourceNotFoundException ex, RedirectAttributes redirectAttributes) {
         String errorId = java.util.UUID.randomUUID().toString(); // Génère un identifiant unique
         logger.error("Ressource introuvable (ID: {}): {}", errorId, ex.getMessage(), ex);
-        redirectAttributes.addFlashAttribute("errorMessage", "La ressource demandée est introuvable. (ID: " + errorId + ")");
+        redirectAttributes.addFlashAttribute("errorMessage", "La ressource demandée est introuvable. (Message d'erreur: " + ex.getMessage()+ ")");
         return "redirect:/ads"; // Redirection vers la liste des annonces
     }
 
@@ -38,16 +37,16 @@ public class GlobalExceptionHandler {
      * Gestion des exceptions de validation
      *
      * @param ex
-     * @param model
+     * @param redirectAttributes
      * @return
      */
     @ExceptionHandler(Exception.class)
-    public String handleGeneralException(Exception ex, Model model) {
+    public String handleGeneralException(Exception ex, RedirectAttributes redirectAttributes) {
 
         // Gestion des autres exceptions générales
         String errorId = java.util.UUID.randomUUID().toString();
         logger.error("Une erreur inattendue est survenue (ID: {}): {}", errorId, ex.getMessage(), ex);
-        model.addAttribute("errorMessage", "Une erreur inattendue est survenue. Veuillez réessayer plus tard. (ID: " + errorId + ")");
+        redirectAttributes.addFlashAttribute("errorMessage", "Une erreur inattendue est survenue. Veuillez réessayer plus tard. (Message d'erreur : " + ex.getMessage() + ")");
         return "redirect:/ads"; // Redirection vers la liste des annonces
     }
 
@@ -55,14 +54,14 @@ public class GlobalExceptionHandler {
      * Gestion des exceptions S3Exception
      *
      * @param ex
-     * @param model
+     * @param redirectAttributes
      * @return
      */
     @ExceptionHandler(S3Exception.class)
-    public String handleS3Exception(S3Exception ex, Model model) {
+    public String handleS3Exception(S3Exception ex, RedirectAttributes redirectAttributes) {
         String errorId = java.util.UUID.randomUUID().toString(); // Génère un identifiant unique
         logger.error("Erreur S3 AWS (ID: {}): {}", errorId, ex.getMessage(), ex);
-        model.addAttribute("errorMessage", "Une erreur est survenue avec le service S3 AWS. Veuillez réessayer plus tard. (ID: " + errorId + ")");
+        redirectAttributes.addFlashAttribute("errorMessage", "Une erreur est survenue avec le service S3 AWS. Veuillez réessayer plus tard. (Message d'erreur : " + ex.getMessage() + ")");
         return "redirect:/ads"; // Redirection vers la liste des annonces
     }
 
